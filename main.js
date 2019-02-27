d9ecead9275dc4963fd75bdf5614a892991841af
techniponi <wincam97@gmail.com>
2019-02-27 13:32:54 -0600

    Major UI overhaul

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 var verboseOutput = true;
 var currentPage = 1;
 
+var grid = $(".grid");
+
+// initialize Masonry
+grid.masonry({
+  // options
+  itemSelector: ".grid-item",
+  columnWidth: 300,
+  gutter: 10
+});
+
 function getSearchQuery() {
+  // clear current grid
+  grid.masonry("remove", grid.find(".grid-item"));
+
   // obtain tag query
   var tags = document.getElementById("tags").value;
-  const div = document.getElementById("results"); // div on page to append results
+  const statusDiv = document.getElementById("status"); // div on page to append results
 
   // Loading indicator
-  div.innerHTML = "<b>Loading, please wait...</b>";
+  statusDiv.innerHTML = "<b>Loading, please wait...</b>";
 
   // make array of tags
   var splitTags = tags.split(" ");
@@ -21,12 +34,18 @@ function getSearchQuery() {
   // check if there are more than 6 tags
   if (splitTags.length > 6) {
     // let user know this is too many tags
-    div.innerHTML =
+    statusDiv.innerHTML =
       "<b>Your fetishes are getting really specific.</b><br/>7+ tag searches are still a work in progress. For now, keep your searches at 6 tags or less!";
   } else {
+    // check desired results size
+    var resultSize = document.getElementById("resultAmount").value;
+    if (resultSize === "") resultSize = 10;
+
     // URL to request results from
     var requestURL =
-      "https://cors-anywhere.herokuapp.com/https://e621.net/post/index.json?limit=20&page=" + // TODO: replace this service with own service
+      "https://cors-anywhere.herokuapp.com/https://e621.net/post/index.json?limit=" +
+      resultSize +
+      "&page=" + // TODO: replace this service with own service
       currentPage +
       "&tags=" +
       tags;
@@ -44,53 +63,45 @@ function getSearchQuery() {
       var results = request.response;
 
       appendResultsToPage(results); // Add results to page
+      statusDiv.innerHTML = "";
     };
   }
 
   // add all results to page
   function appendResultsToPage(resultsArray) {
-    // padding and functionality variables
-
-    div.innerHTML = "Results:<br />"; // clean up any existing results
-    resultsArray.forEach(function(result, resultIndex) {
+    resultsArray.forEach(function (result) {
       // convenience variables
       const fileUrl = result["file_url"];
       const fileName = result["artist"] + " - " + result["md5"];
       const fileType = result["file_ext"];
+      const authorName = result["artist"];
+      const pageUrl = "https://e621.net/post/show/" + result["id"];
 
       verboseLog("Appending image:\n" + fileUrl + "\n" + fileName);
 
       // check if file is an SWF or WEBM
       if (fileType === "webm") {
-        // add red outline to indicate that this is a webm
-        div.innerHTML +=
-          '<a href="' +
-          fileUrl +
-          '"><video src="' +
-          result["file_url"] +
-          '" title="' +
-          result["tags"] +
-          '">    </a>';
+        // TODO: implement webm functionality for masonry
+        // asdf
       } else if (fileType === "swf") {
-        // use swf-icon.png to indicate that this is an swf
-        div.innerHTML +=
-          '<a href="' +
-          fileUrl +
-          '"><img src="swf-icon.png" title="' +
-          result["tags"] +
-          '">    </a>';
+        // TODO: implement swf functionality for masonry
+        // blegh
       } else {
         // this is an image
-        div.innerHTML +=
-          '<a href="' +
-          fileUrl +
-          '"><img src="' +
-          result["file_url"] +
-          '" title="' +
-          result["tags"] +
-          '" alt="' +
-          resultIndex +
-          '">    </a>';
+        var link = document.createElement("a");
+        link.href = fileUrl;
+        var image = document.createElement("img");
+        image.classList.add("grid-item");
+        image.src = fileUrl;
+        link.appendChild(image);
+
+        $(".grid")
+          .append(link)
+          .masonry("appended", link)
+          .imagesLoaded()
+          .progress(function () {
+            $(".grid").masonry();
+          });
       }
     });
   }
@@ -99,6 +110,29 @@ function getSearchQuery() {
 /*
 // GENERAL HELPER FUNCTIONS
 */
+
+// create HTML code for a card object
+function createCard(imageUrl, authorName, pageUrl) {
+  var htmlText =
+    "" +
+    '<div class="grid-item" style="float: left;">' +
+    '<div class="card" >' +
+    '<div class="card-image waves-effect waves-block waves-light">' +
+    "<img class='activator' src='" +
+    imageUrl +
+    "'>" +
+    "</div>" +
+    "<div class='card-content'>" +
+    "<span class='card-title activator grey-text text-darken-4'>" +
+    authorName +
+    '<i class="material-icons right">more_vert</i></span>' +
+    '<p><a href="' +
+    pageUrl +
+    '">Page</a></p>' +
+    "</div></div></div>";
+
+  return htmlText;
+}
 
 // advance to next page and automatically reload results
 function pageNext() {

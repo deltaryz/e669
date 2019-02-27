1dfb26791709c27199dc0f8a9ab37e10276628b3
techniponi <wincam97@gmail.com>
2019-02-27 09:14:53 -0600

    yeet

--- main.js
+++ main.js
@@ -1,51 +1,99 @@
 var verboseOutput = true;
+var currentPage = 1;
 
 function getSearchQuery() {
-  // URL to request results from
-  var requestURL = "https://cors.io/?https://e621.net/post/index.json?limit=10"; // TODO: replace this with own service
+  // obtain tag query
+  var tags = document.getElementById("tags").value;
 
-  // create request
-  verboseLog("creating request");
-  var request = new XMLHttpRequest();
-  request.open("GET", requestURL);
-  request.responseType = "json";
-  request.send();
+  // make array of tags
+  var splitTags = tags.split(" ");
+  verboseLog(
+    "User searched with tags:\n" +
+      splitTags +
+      "\nArray length: " +
+      splitTags.length
+  );
 
-  // once request loads
-  request.onload = function() {
-    verboseLog("Request has loaded");
-    var results = request.response;
-    appendResultsToPage("results", results); // Add results to page
-  };
-}
+  // check if there are more than 6 tags
+  if (splitTags.length > 6) {
+    // let user know this is too many tags
+    var div = document.getElementById("results");
+    div.innerHTML =
+      "<b>Your fetishes are getting really specific.</b><br/>7+ tag searches are still a work in progress. For now, keep your searches at 6 tags or less!";
+  } else {
+    // URL to request results from
+    var requestURL =
+      "https://cors-anywhere.herokuapp.com/https://e621.net/post/index.json?limit=10&page=" +
+      currentPage +
+      "&tags=" +
+      tags; // TODO: replace this with own service
 
-// add all results to page
-function appendResultsToPage(divId, resultsArray) {
-  // padding and functionality variables
+    // create request
+    verboseLog("creating request to " + requestURL);
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
 
-  var div = document.getElementById(divId); // div on page to append results
-  div.innerHTML = "Results:<br />"; // clean up any existing results TODO: make this its own function
-  resultsArray.forEach(function(result, resultIndex) {
-    const fileUrl = result["file_url"];
-    const fileName = result["artist"] + " - " + result["md5"];
+    // once request loads
+    request.onload = function() {
+      verboseLog("Request has loaded");
+      var results = request.response;
+      appendResultsToPage("results", results); // Add results to page
+    };
+  }
 
-    verboseLog("Appending image:\n" + fileUrl + "\n" + fileName);
+  // add all results to page
+  function appendResultsToPage(divId, resultsArray) {
+    // padding and functionality variables
 
-    div.innerHTML +=
-      resultIndex +
-      ': <a href="' +
-      fileUrl +
-      '"><img src="' +
-      result["file_url"] +
-      '" alt="' +
-      result["tags"] +
-      '"></a><br />';
-  });
+    var div = document.getElementById(divId); // div on page to append results
+    div.innerHTML = "Results:<br />"; // clean up any existing results TODO: make this its own function
+    resultsArray.forEach(function(result, resultIndex) {
+      const fileUrl = result["file_url"];
+      const fileName = result["artist"] + " - " + result["md5"];
+
+      verboseLog("Appending image:\n" + fileUrl + "\n" + fileName);
+
+      div.innerHTML +=
+        '<a href="' +
+        fileUrl +
+        '"><img src="' +
+        result["file_url"] +
+        '" title="' +
+        result["tags"] +
+        '" alt="' +
+        resultIndex +
+        '">    </a>';
+    });
+  }
 }
 
 /*
 // GENERAL HELPER FUNCTIONS
 */
+
+// advance to next page and automatically reload results
+function pageNext() {
+  verboseLog("User is moving to next page");
+  currentPage++;
+  updatePageNumber();
+  getSearchQuery();
+}
+
+// go to previous page and automatically reload results
+function pagePrevious() {
+  verboseLog("User is moving to previous page");
+  if (currentPage > 1) currentPage--;
+  updatePageNumber();
+  getSearchQuery();
+}
+
+// update the displayed page number
+function updatePageNumber() {
+  pageNumberElement = document.getElementById("pageNumber").innerText =
+    "Page " + currentPage;
+}
 
 // Print to console only if verbose output is enabled
 function verboseLog(text) {
